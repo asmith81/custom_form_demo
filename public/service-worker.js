af662de8d202acd57d18f7a85ca5ff1f9900f737
Alden Smith <alden.w.smith.iii@gmail.com>
2025-11-17 18:22:04 -0500

    Bump service worker cache version to force update

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@
  * Caches static assets for offline availability
  */
 
-const CACHE_NAME = 'jobsite-form-v1';
+const CACHE_NAME = 'jobsite-form-v2';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
